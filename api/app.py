9eabe1d996d6c29a71a9b63d9c591805e41eb6a5
paul <112290055+2560p@users.noreply.github.com>
2023-06-07 00:13:51 +0200

    API: make use of .env file
    This allows to store important data (API keys) without uploading them to
    GitHub.
    
    Signed-off-by: paul <112290055+2560p@users.noreply.github.com>

--- api/app.py
+++ api/app.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
+
 from flask import Flask, request
 
 app = Flask(__name__)
+load_dotenv()
 
 @app.route('/')
 def hello():
@@ -11,7 +15,17 @@ def hello():
 def param(custom):
     return "your submitted parameter is " + custom
 
+
 @app.route('/query')
 def return_data():
     args = request.args
     return "you have submitted this query: " + str(dict(args))
+
+@app.route('/secret')
+def secret():
+    secret = os.environ.get('SECRET')
+    return "the server secret is " + secret
+
+@app.route('/post', methods=['POST'])
+def post():
+    return "you have submitted the following data: " + str(dict(request.values)) + "\n"
