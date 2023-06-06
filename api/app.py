5abbb17a5baf1903bfdb8e769579a52d8f448164
paul <112290055+2560p@users.noreply.github.com>
2023-06-06 21:17:30 +0200

    API: testing Flask capabilities
    This just brings another endpoint of '/param/' which returns some text
    with anything that comes after the second slash.
    
    Signed-off-by: paul <112290055+2560p@users.noreply.github.com>

--- api/app.py
+++ api/app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "hello, world!"
+
+
+@app.route('/param/<custom>')
+def param(custom):
+    return "your submitted parameter is " + custom
+
