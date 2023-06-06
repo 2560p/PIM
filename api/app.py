87ea9c029617dfb88ee40a0a13b7f67de7f6d804
paul <112290055+2560p@users.noreply.github.com>
2023-06-06 23:43:52 +0200

    API: get GET params
    Signed-off-by: paul <112290055+2560p@users.noreply.github.com>

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -11,3 +11,7 @@ def hello():
 def param(custom):
     return "your submitted parameter is " + custom
 
+@app.route('/query')
+def return_data():
+    args = request.args
+    return "you have submitted this query: " + str(dict(args))
