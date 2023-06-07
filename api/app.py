dd029d2eef06c1ac831c1cb0e92b9daadab619df
paul <112290055+2560p@users.noreply.github.com>
2023-06-07 21:23:54 +0200

    API: blueprint division
    This helps organising the code. It is now possible to divide the
    functions between different files, which should improve readability.
    
    Signed-off-by: paul <112290055+2560p@users.noreply.github.com>

--- api/app.py
+++ api/app.py
@@ -1,31 +1,9 @@
-import os
-from dotenv import load_dotenv
+from flask import Flask
 
-from flask import Flask, request
+from routes.secret import secret_page
+from routes.post import post_page
 
 app = Flask(__name__)
-load_dotenv()
 
-@app.route('/')
-def hello():
-    return "hello, world!"
-
-
-@app.route('/param/<custom>')
-def param(custom):
-    return "your submitted parameter is " + custom
-
-
-@app.route('/query')
-def return_data():
-    args = request.args
-    return "you have submitted this query: " + str(dict(args))
-
-@app.route('/secret')
-def secret():
-    secret = os.environ.get('SECRET')
-    return "the server secret is " + secret
-
-@app.route('/post', methods=['POST'])
-def post():
-    return "you have submitted the following data: " + str(dict(request.values)) + "\n"
+app.register_blueprint(secret_page, url_prefix='/secret')
+app.register_blueprint(post_page, url_prefix='/post')
