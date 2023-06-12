f2f8b67221498542dcdbc58ec2b3625d942a8f13
GabrielGuevaraLopez <114413163+GabrielGuevaraLopez@users.noreply.github.com>
2023-06-12 12:44:58 +0200

    worked and tested stt function

--- api/routes/stt.py
+++ api/routes/stt.py
@@ -1,6 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from responses import ok, err, server_err
 
+import openai
+from dotenv import load_dotenv
+import os
+import io
+
+load_dotenv()
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 stt_page = Blueprint('stt_page', __name__)
+# stt_page.config = {}
+# stt_page.config['UPLOAD_EXTENSIONS'] = ['.m4a']
 
+@stt_page.route('', methods=["POST"])
+def transcribe():    
+  try:
+    # file = request.files['file']
+    file = io.BytesIO(request.data)
+    file.name = "something.mp3"
 
+    result = openai.Audio.transcribe("whisper-1", file).text
+    return ok(result)
+  except Exception as err:
+    return err(err)
