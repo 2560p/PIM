2e1a040c34dd2d9655e5e68ba544403447b5322c
paul <112290055+2560p@users.noreply.github.com>
2023-06-12 19:26:01 +0200

    fixup! cleanup: updated .gitignore, deleted cache

--- api/app.py
+++ api/app.py
@@ -1,8 +1,5 @@
 from flask import Flask
 
-from routes.secret import secret_page
-from routes.post import post_page
-from routes.test import test_page
 from routes.stt import stt_page
 from routes.conversation import conversation_page
 from routes.translate import translate_page
@@ -10,9 +7,6 @@ from routes.tts import tts_page
 
 app = Flask(__name__)
 
-app.register_blueprint(secret_page, url_prefix='/secret')
-app.register_blueprint(post_page, url_prefix='/post')
-app.register_blueprint(test_page, url_prefix='/test')
 app.register_blueprint(stt_page, url_prefix='/stt')
 app.register_blueprint(conversation_page, url_prefix='/conversation')
 app.register_blueprint(translate_page, url_prefix='/translate')
--- api/routes/post.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request
-
-post_page = Blueprint('post_page', __name__)
-
-@post_page.route('', methods=['POST'])
-def post():
-    return "post data: " + str(dict(request.values)) + "\n"
