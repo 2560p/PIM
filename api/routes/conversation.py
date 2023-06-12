022692a04eb346d4786892cd94dc093e3a1e6620
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2023-06-12 14:35:15 +0200

    submitted changes according to the pull request

--- api/routes/conversation.py
+++ api/routes/conversation.py
@@ -2,26 +2,24 @@ from flask import Blueprint, request
 
 import os
 import openai
-# from responses import ok, err, server_err
+from responses import ok, err, server_err
 
 from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-conversation_page = Blueprint('conversation', __name__)
+conversation_page = Blueprint('conversation_page', __name__)
 
 
 @conversation_page.route('', methods=['POST'])
 def post():
     prompt = request.form.get('text')
 
-    completion = openai.ChatCompletion.create(
+    response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
             {"role": "user", "content": prompt}
         ]
     )
 
-    # print(completion.choices[0].message["content"])
-
-    return completion.choices[0].message["content"]
+    return ok(response.choices[0].message["content"])
