5a1886522d47a9b8b931d5e0cd64228a63b5354d
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2023-06-12 12:08:09 +0200

    added the conversation file

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from routes.secret import secret_page
 from routes.post import post_page
 from routes.test import test_page
 from routes.stt import stt_page
+from routes.conversation import conversation_page
 
 app = Flask(__name__)
 
@@ -11,3 +12,4 @@ app.register_blueprint(secret_page, url_prefix='/secret')
 app.register_blueprint(post_page, url_prefix='/post')
 app.register_blueprint(test_page, url_prefix='/test')
 app.register_blueprint(stt_page, url_prefix='/stt')
+app.register_blueprint(conversation_page, url_prefix='/conversation')
