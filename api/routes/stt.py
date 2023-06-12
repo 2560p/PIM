35436a3c72e70f4336ab5bc159f954c2c4fa813e
GabrielGuevaraLopez <114413163+GabrielGuevaraLopez@users.noreply.github.com>
2023-06-12 14:21:03 +0200

    Fixed small issues for stt.py

--- api/routes/stt.py
+++ api/routes/stt.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, request
 from responses import ok, err, server_err
+from dotenv import load_dotenv
 
 import openai
-from dotenv import load_dotenv
 import os
 import io
 
@@ -10,13 +10,10 @@ load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 stt_page = Blueprint('stt_page', __name__)
-# stt_page.config = {}
-# stt_page.config['UPLOAD_EXTENSIONS'] = ['.m4a']
 
 @stt_page.route('', methods=["POST"])
 def transcribe():    
   try:
-    # file = request.files['file']
     file = io.BytesIO(request.data)
     file.name = "something.mp3"
 
