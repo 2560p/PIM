2e563c1d22669d8b7dd2ea3b9dddc6fc177a1d44
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2023-06-12 12:12:25 +0200

    used openai to generate a response

--- api/routes/conversation.py
+++ api/routes/conversation.py
@@ -9,3 +9,18 @@ load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 conversation = Blueprint('conversation', __name__)
+
+prompt = "hello"
+
+# Generate a completion
+response = openai.Completion.create(
+    engine='davinci-codex',
+    prompt=prompt,
+    max_tokens=100
+)
+
+# Extract the generated text from the response
+generated_text = response.choices[0].text.strip()
+
+# Print the generated completion
+print(generated_text)
