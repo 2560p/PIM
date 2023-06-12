e9d9639ca78db316de1ccff63f1187c7d15edf6f
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-06-12 14:25:42 +0200

    made changes requested

--- api/app.py
+++ api/app.py
@@ -3,22 +3,15 @@ from flask import Flask
 from routes.secret import secret_page
 from routes.post import post_page
 from routes.test import test_page
-<<<<<<< HEAD
 from routes.stt import stt_page
 from routes.conversation import conversation_page
-=======
 from routes.translate import translate_page
->>>>>>> 57a210e (finished translate part)
 
 app = Flask(__name__)
 
 app.register_blueprint(secret_page, url_prefix='/secret')
 app.register_blueprint(post_page, url_prefix='/post')
 app.register_blueprint(test_page, url_prefix='/test')
-<<<<<<< HEAD
 app.register_blueprint(stt_page, url_prefix='/stt')
 app.register_blueprint(conversation_page, url_prefix='/conversation')
-=======
-
 app.register_blueprint(translate_page, url_prefix='/translate')
->>>>>>> 57a210e (finished translate part)
