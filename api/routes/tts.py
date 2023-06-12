d0c3edc050b0941f9c90cf0b84cc395db0ca8924
Marijn Veenstra <114413125+MarijnVeenstra@users.noreply.github.com>
2023-06-12 14:06:48 +0200

    Implemented text to speech functionality

--- api/routes/tts.py
+++ api/routes/tts.py
@@ -1,42 +1,42 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, send_file
 from responses import ok, err, server_err
+import requests
+import io
 
 tts_page = Blueprint('tts_page', __name__)
 
-
 @tts_page.route('', methods=["POST"])
-def tts(text):
-  #text is the text thats gonna be turned into speech using 11labs.
-  #return ok("yey")
-  #PostForm needs to be changed to the variable which holds either nl or en, since we ened to know whether the text is in Dutch or English
-if PostForm == 'en':
-  api_key = 'MY KEY'
+def tts(): 
+  lang = request.form.get("lang")
+  text = request.form.get("text")
 
-  api_url = "https://api.elevenlabs.ai/speech"
+  if lang == "en":
+   # This is normal Marijn
+    url = "https://api.elevenlabs.io/v1/text-to-speech/xEucmSu4xLB83D3WYWPa"
+   
+   # This is Angry/screaming Marijn
+   # url = "https://api.elevenlabs.io/v1/text-to-speech/5fXNDVjFv4jo0c9LTZrT"
+    
+    headers = {
+      'xi-api-key': '',
+      'Content-Type': 'application/json'
+    }
 
-  headers = {
-    'Authorization': f'Bearer {api_key}',
-    'Content-Type': 'application/json'
-  }
-
-  payload = {
-    "text" = text,
-    "voice_id": "xEucmSu4xLB83D3WYWPa",
-    "model_id": "eleven_monolingual_v1",
-    "voice_settings": {
-      "stability": 75,
-      "similarity_boost": 75
+    payload = {
+      "text": text,
+      "model_id": "eleven_monolingual_v1",
+      "voice_settings": {
+        "stability": 0.75,
+        "similarity_boost": 0.75
     }
   }
-  
-  response = requests.post(api_url, json=payload, headers=headers)
-
-
-
-
-  
- 
-  if PostForm == 'nl':
-    response = requests.post(api_url, json=payload)
     
-    
+    resp = requests.post(url, json=payload, headers=headers)
+
+    file = io.BytesIO(resp.content)
+    # file.name = "test.mp3"
+    return send_file(file, mimetype="audio/mpeg", download_name='file.mp3')
+  else:
+    return err("lang not supported (yet)")
+
+  
