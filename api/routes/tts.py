7c9c36b6f5dc63d953ef4667fd126a4aacd2a726
Marijn Veenstra <114413125+MarijnVeenstra@users.noreply.github.com>
2023-06-13 18:17:39 +0200

    removed spaces, reformatted the exceptions

--- api/routes/tts.py
+++ api/routes/tts.py
@@ -11,22 +11,19 @@ tts_page = Blueprint('tts_page', __name__)
 
 @tts_page.route('', methods=["POST"])
 def tts(): 
-  if "lang" in request.form and "text" in request.form:
+  if "lang" not in request.form and "text" not in request.form:
+    return err("The language and text have not been provided")
+  elif "lang" not in request.form:
+    return err("The language has not been provided")
+  elif "text" not in request.form:
+    return err("The text has not been provided")
+  else:
     lang = request.form.get("lang")
     text = request.form.get("text")
-  else:
-    if "lang" not in request.form and "text" not in request.form:
-      return err("The language and text have not been provided")
-    elif "lang" not in request.form:
-      return err("The language has not been provided")
-    elif "text" not in request.form:
-      return err("The text has not been provided")
-    else:
-      return server_err("I have no clue what happened. You shouldn't see this error.")
-  
+
 # This is normal Marijn
   url = "https://api.elevenlabs.io/v1/text-to-speech/xEucmSu4xLB83D3WYWPa"
-  
+
   headers = {
     'xi-api-key': os.environ.get("ELEVENLABS_API_KEY"),
     'Content-Type': 'application/json'
@@ -41,19 +38,17 @@ def tts():
     }
   }
 
-  if lang == "en" and text != "":
+  if lang == "" and text == "":
+    return err("No language specified and no text has been provided to turn into speech")
+  elif lang != "en" and text == "":
+    return err("Language not supported yet and no text has been provivided to turn into speech")
+  elif lang == "":
+    return err("No lnaguage specified")
+  elif lang != "en":
+    return err("Language not supported")
+  elif text == "":
+    return err("No text has been filled in to be turned into speech")
+  else:
     resp = requests.post(url, json=payload, headers=headers)
     file = io.BytesIO(resp.content)
     return send_file(file, mimetype="audio/mpeg", download_name='file.mp3')
-  
-  else:
-    if lang == "" and text == "":
-      return err("No language specified and no text has been provided to turn into speech")
-    elif lang != "en" and text == "":
-      return err("Language not supported yet and no text has been provivided to turn into speech")
-    elif lang == "":
-      return err("No lnaguage specified")
-    elif lang != "en":
-      return err("Language not supported")
-    if text == "":
-      return err("No text has been filled in to be turned into speech")
