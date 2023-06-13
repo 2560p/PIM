6f8dfa04a7b7d562086ae1ebb246aa408b9122db
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-06-13 17:22:57 +0200

    finished error handling

--- api/routes/translate.py
+++ api/routes/translate.py
@@ -2,8 +2,8 @@ from flask import Blueprint, request
 import openai
 import os
 from dotenv import load_dotenv
-
-from responses import ok
+import requests
+from responses import ok, err, server_err
 
 translate_page = Blueprint('translate_page', __name__)
 
@@ -13,20 +13,21 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 @translate_page.route('', methods=['POST'])
 def post():
-    text = request.form.get('text')
+    prompt = request.form.get('text')
 
-    translation = translate_text(text)
-    return ok(translation)
+    if not prompt:
+        return err("Please input text to be translated")
 
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system",
+                 "content": "You are an assistant with translation to Dutch. Your name is Pim. You should always respond with \"Alsjeblieft!\" when someone thanks you. Otherwise, respond with a Dutch translation of the sentence. Make sure that the level of Dutch is basic, so that even a beginner can understand."},
+                {"role": "system", "content": prompt},
+            ]
+        )
+    except:
+        return server_err("Error occurred during API response")
 
-def translate_text(text):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system",
-             "content": "You are an assistant with translation to Dutch. Your name is Pim. You should always respond with \"Alsjeblieft!\" when someone thanks you. Otherwise, respond with a Dutch translation of the sentence. Make sure that the level of Dutch is basic, so that even a beginner can understand."},
-            {"role": "system", "content": text},
-        ]
-    )
-
-    return response.choices[0].message.content
+    return ok(response.choices[0].message.content)
