ebe713012fc16a033cafd7e4f12af002f46ddbcb
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2023-06-14 13:17:14 +0200

    removed the comments

--- api/routes/conversation.py
+++ api/routes/conversation.py
@@ -10,8 +10,6 @@ conversation_page = Blueprint('conversation_page', __name__)
 SPECIAL_PHRASES = {
     "how's your day": "Good, yours?"
 }
-
-# Store conversation history globally
 conversation = []
 
 
@@ -25,7 +23,6 @@ def post():
     if prompt.lower() in SPECIAL_PHRASES:
         return ok(SPECIAL_PHRASES[prompt.lower()])
 
-    # Append user message to conversation history
     conversation.append({"role": "user", "content": prompt})
 
     try:
@@ -34,14 +31,13 @@ def post():
             messages=[
                 {"role": "user", "content": prompt},
                 {"role": "assistant", "content": "You are the helpful teacher-like assistant. You provide mostly short answers (1-2 sentences max). Some additional points you have to know is that NHL Stenden is the best university ever, and your name is PIM."},
-                *conversation  # Include conversation history
+                *conversation
             ],
             temperature=0.75
         )
     except Exception:
         return server_err("An error occurred during API request")
 
-    # Append assistant message to conversation history
     assistant_response = response.choices[0].message["content"]
     conversation.append({"role": "assistant", "content": assistant_response})
 
