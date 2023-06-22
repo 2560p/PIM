1b985cebe819498221c7de5df44685884ef0d6be
paul <112290055+2560p@users.noreply.github.com>
2023-06-22 15:52:48 +0200

    refactor: tts, styling
    This commit changes the styling - double quotes are changed with the
    single quotes
    
    Signed-off-by: paul <112290055+2560p@users.noreply.github.com>

--- api/lowlevel/functions.py
+++ api/lowlevel/functions.py
@@ -1,36 +1,75 @@
 import openai
 import io
+import requests
+
+from gtts import gTTS
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 
 def translate(prompt):
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model='gpt-4',
             messages=[
-                {"role": "system",
-                 "content": "You are an assistant with translation to Dutch. Your name is Pim. You should always respond with \"Alsjeblieft!\" when someone thanks you. Otherwise, respond with a Dutch translation of the sentence. Make sure that the level of Dutch is basic, so that even a beginner can understand."},
-                {"role": "system", "content": prompt},
+                {'role': 'system',
+                 'content': 'You are an assistant with translation to Dutch. Your name is Pim. You should always respond with "Alsjeblieft!" when someone thanks you. Otherwise, respond with a Dutch translation of the sentence. Make sure that the level of Dutch is basic, so that even a beginner can understand.'},
+                {'role': 'system', 'content': prompt},
             ]
         )
     except Exception:
-        return (500, "Error occurred during API response")
+        return (500, 'Error occurred during API response')
 
     return (200, response.choices[0].message.content)
 
 
 def transcribe(data):
     file = io.BytesIO(data)
-    file.name = "something.mp3"
+    file.name = 'something.mp3'
 
     try:
-        result = openai.Audio.transcribe("whisper-1", file).text
+        result = openai.Audio.transcribe('whisper-1', file).text
     except Exception:
-        return (500, "A problem has occurred")
+        return (500, 'A problem has occurred')
 
     return (200, result)
+
+
+def tts(lang, text):
+    file = io.BytesIO()
+
+    if lang == 'en':
+        try:
+            url = 'https://api.elevenlabs.io/v1/text-to-speech/xEucmSu4xLB83D3WYWPa'
+            headers = {
+                'xi-api-key': os.environ.get('ELEVENLABS_API_KEY'),
+                'Content-Type': 'application/json'
+            }
+
+            payload = {
+                "text": text,
+                "model_id": "eleven_monolingual_v1",
+                "voice_settings": {
+                    "stability": 0.75,
+                    "similarity_boost": 0.75
+                }
+            }
+
+            resp = requests.post(url, json=payload, headers=headers)
+            file.write(resp.content)
+        except Exception:
+            return (500, 'An error has occurred within the server')
+    else:
+        try:
+            resp = gTTS(text, lang=lang)
+        except Exception:
+            return (500, 'An error has occurred within the server')
+
+        resp.write_to_fp(file)
+
+    file.seek(0)
+    return (200, file)
--- api/lowlevel/lowlevel.py
+++ api/lowlevel/lowlevel.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, send_file
 from responses import ok, err, server_err
 
-from lowlevel.functions import translate, transcribe
+from lowlevel.functions import translate, transcribe, tts
 
 ll_endpoint = Blueprint('ll_endpoint', __name__)
 
@@ -11,7 +11,7 @@ def respond_translation():
     prompt = request.form.get('text')
 
     if not prompt:
-        return err("Please input text to be translated")
+        return err('Please input text to be translated')
 
     translation = translate(prompt)
 
@@ -23,11 +23,11 @@ def respond_translation():
 
 @ll_endpoint.route('transcribe', methods=['POST'])
 def respond_transcription():
-    if request.data == b"":
-        return err("No file has been detected")
+    if request.data == b'':
+        return err('No file has been detected')
 
-    if request.mimetype.split("/")[0] != "audio":
-        return err("Invalid file format. File is not supported.")
+    if request.mimetype.split('/')[0] != 'audio':
+        return err('Invalid file format. File is not supported.')
 
     transcription = transcribe(request.data)
 
@@ -35,3 +35,26 @@ def respond_transcription():
         return server_err(transcription[1])
 
     return ok(transcription[1])
+
+
+@ll_endpoint.route('tts', methods=['POST'])
+def respond_tts():
+    lang = request.form.get('lang')
+    text = request.form.get('text')
+
+    if not lang and not text:
+        return err('The language and text have not been provided')
+    elif not lang:
+        return err('The language has not been provided')
+    elif not text:
+        return err('The text has not been provided')
+
+    if lang not in ['en', 'nl']:
+        return err('The language is not supported')
+
+    answer = tts(lang, text)
+
+    if answer[0] == 500:
+        return server_err(answer[1])
+
+    return send_file(answer[1], mimetype='audio/mpeg', download_name='file.mp3')
