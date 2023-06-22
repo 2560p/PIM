050a5c153873e41d0ace93e3ba16fde04f59fc2a
2560p <112290055+2560p@users.noreply.github.com>
2023-06-22 22:34:00 +0200

    Refactor (#21)
    This commit is a squash commit; it includes all the changes from the refactoring.

--- api/app.py
+++ api/app.py
@@ -1,15 +1,6 @@
 from flask import Flask
-
-from routes.transcribe import transcribe_page
-from routes.conversation import conversation_page
-from routes.translate import translate_page
-from routes.tts import tts_page
-from routes.quiz import quiz_page
+from lowlevel.lowlevel import ll_endpoint
 
 app = Flask(__name__)
 
-app.register_blueprint(transcribe_page, url_prefix='/transcribe')
-app.register_blueprint(conversation_page, url_prefix='/conversation')
-app.register_blueprint(translate_page, url_prefix='/translate')
-app.register_blueprint(tts_page, url_prefix="/tts")
-app.register_blueprint(quiz_page, url_prefix="/quiz")
+app.register_blueprint(ll_endpoint, url_prefix='/ll')
