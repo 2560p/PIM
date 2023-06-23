a1853cb8bd81864c4e085a93e59f0b2860eff55d
paul <112290055+2560p@users.noreply.github.com>
2023-06-23 14:35:55 +0200

    pim.py: sending the request to the server
    Signed-off-by: paul <112290055+2560p@users.noreply.github.com>

--- hardware/scripts/pim.py
+++ hardware/scripts/pim.py
@@ -1,13 +1,23 @@
 import time
 import speech_recognition as sr
+import requests
 
 
 def callback(recognizer, audio):
     print('called callback')
+    with open('audio/audio.m4a', 'rb') as file:  # direct input from the mic does not work
+        try:
+            print('got the message, awaiting the response.')
+            answer = requests.post('http://127.0.0.1:5000/pim/conversation',
+                                   data=file.read(),
+                                   headers={'Content-Type': 'audio/wav'})
+            assert answer.status_code == 200
+        except Exception as e:
+            print('whoopsie: ' + str(e) + '. ' + answer[1])
 
 
 r = sr.Recognizer()
-m = sr.Microphone(device_index=0)
+m = sr.Microphone()
 
 stop_listening = r.listen_in_background(m, callback)
 
